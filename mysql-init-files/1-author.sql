4b9ecdf6b0c02ccad828011f05d588b9c9125afd
Arrowzi <boriska1995.02@gmail.com>
2020-12-28 21:49:46 +0500

    inserts

---
+++ mysql-init-files/1-author.sql
@@ -0,0 +1,8 @@
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (2, 'Иван', 'Иванов', '2000-04-21');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (3, 'Владислав', 'Чуев', '2000-11-05');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (4, 'Дмитрий', 'Николаев', '1980-05-12');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (5, 'Кирилл', 'Петров', '1995-12-22');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (6, 'Лев', 'Давыдычев', '1927-01-01');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (7, 'Николай ', 'Рубцов', '1936-01-03');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (8, 'Анатолий', 'Рыбаков', '1931-01-14');
+INSERT INTO bookmarket.author (id, fname, sname, dob) VALUES (9, 'Пётр ', 'Ершов', '1815-03-06');
